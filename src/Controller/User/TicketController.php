683339d68c05d2c0dfdefc88e397356f64b80aa8
beatercode <cristinabova.998@gmail.com>
2024-02-08 18:12:33 +0100

    ticket creation logic implementation

--- src/Controller/User/TicketController.php
+++ src/Controller/User/TicketController.php
@@ -3,20 +3,27 @@
 namespace App\Controller\User;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\UserRepository;
+use App\Entity\Ticket;
+use App\Entity\User;
 
 class TicketController extends AbstractController
 {
-    #[Route('/ticket', name: 'app_ticket')]
-    public function index(UserRepository $userRepository): Response
-    {
-        $user = $userRepository->find(1);
+    private $entityManager;
 
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/ticket', name: 'app_ticket')]
+    public function index(): Response
+    {
         return $this->render('user/ticket/index.html.twig', [
             'controller_name' => 'TicketController',
-            'user' => $user, 
         ]);
     }
 
@@ -24,5 +31,40 @@ class TicketController extends AbstractController
     public function create(): Response
     {
         return $this->render('user/ticket/create.html.twig');
+    }
+
+    #[Route('/store-ticket', name: 'store_ticket')]
+    public function store(Request $request): Response
+    {
+        if ($request->isMethod('POST')) {
+            $userRepository = $this->entityManager->getRepository(User::class);
+            $user = $userRepository->find(1);
+    
+            $ticket = new Ticket();
+    
+            $ticket->setUser($user);
+    
+            // Ottieni il messaggio 
+            $message = $request->request->get('message');
+    
+            // Verifica se il messaggio è stato fornito
+            if ($message === null) {
+                throw $this->createNotFoundException('Il campo messaggio è obbligatorio.');
+            }
+    
+            // Imposta il messaggio
+            $ticket->setMessage($message);
+    
+            // Imposta data
+            $now = new \DateTimeImmutable();
+            $ticket->setCreatedAt($now);
+            $ticket->setUpdatedAt($now);
+    
+            // Salva ticket nel db
+            $this->entityManager->persist($ticket);
+            $this->entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_ticket');
     }
 }
---
+++ src/Entity/Ticket.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TicketRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TicketRepository::class)]
+class Ticket
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    #[ORM\JoinColumn(name: 'user_id', referencedColumnName: 'id', onDelete: 'SET NULL')]
+    private $user;
+
+    #[ORM\Column(length: 255)]
+    private ?string $message = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updated_at = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): static
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): static
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
