29b895c7116a08394bac65e794ce16c308f8d7f4
beatercode <cristinabova.998@gmail.com>
2024-02-08 15:16:47 +0100

    create page

--- src/Controller/User/TicketController.php
+++ src/Controller/User/TicketController.php
@@ -12,12 +12,17 @@ class TicketController extends AbstractController
     #[Route('/ticket', name: 'app_ticket')]
     public function index(UserRepository $userRepository): Response
     {
-
         $user = $userRepository->find(1);
 
         return $this->render('user/ticket/index.html.twig', [
             'controller_name' => 'TicketController',
             'user' => $user, 
         ]);
+    }
+
+    #[Route('/create-ticket', name: 'create_ticket')]
+    public function create(): Response
+    {
+        return $this->render('user/ticket/create.html.twig');
     }
 }
