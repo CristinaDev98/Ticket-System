f69792f271597515374cde6c2ea260147d260d81
beatercode <cristinabova.998@gmail.com>
2024-02-08 12:36:39 +0100

    dashboard user

--- src/Controller/User/TicketController.php
+++ src/Controller/User/TicketController.php
@@ -5,14 +5,19 @@ namespace App\Controller\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
 
 class TicketController extends AbstractController
 {
     #[Route('/ticket', name: 'app_ticket')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+
+        $user = $userRepository->find(1);
+
         return $this->render('user/ticket/index.html.twig', [
             'controller_name' => 'TicketController',
+            'user' => $user, 
         ]);
     }
 }
